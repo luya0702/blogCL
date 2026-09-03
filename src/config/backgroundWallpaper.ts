3f236c2902b4f2fa8403811291ed9336d2609052
luya0702 <2976977535@qq.com>
2026-09-03 13:48:37 +0800

    Update backgroundWallpaper.ts

--- src/config/backgroundWallpaper.ts
+++ src/config/backgroundWallpaper.ts
@@ -42,6 +42,8 @@ export const backgroundWallpaper: BackgroundWallpaperConfig = {
 			"assets/images/DesktopWallpaper/P1.avif",
 			"assets/images/DesktopWallpaper/P2.avif",
 			"assets/images/DesktopWallpaper/P3.avif",
+			"assets/images/DesktopWallpaper/P4.avif",
+			"assets/images/DesktopWallpaper/P5.avif",
 		],
 		// 移动背景图片（支持单张或多张随机）
 		// mobile: "assets/images/MobileWallpaper/m1.avif",
